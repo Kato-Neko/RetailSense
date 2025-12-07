6d71242cda04abf57bff34d0bd8db7a235f85d4f
jrllejane <jierellejane.ravanes@cit.edu>
2025-12-07 15:34:14 +0800

    api_base_url updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://retailsense-production1.up.railway.app";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://retailsense-production-8a07.up.railway.app";
 export default API_BASE_URL; 
